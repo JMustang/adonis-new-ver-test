1b8aaeefa5b96826ce99f11ce9f877d9b18ed7d2
JMustang <eecfredes@gmail.com>
2021-05-30 16:56:44 -0300

    feat:model create

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,8 +1,21 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
 
 export default class UsersController {
   public async create({ request }: HttpContextContract) {
-    const data = request.only(["name", "username"]);
-    console.log(data);
+    const { name, username } = request.only(["name", "username"]);
+
+    const user = await User.create({
+      username,
+      name,
+    });
+
+    return user;
+  }
+
+  public async index() {
+    const all = await User.all();
+
+    return all;
   }
 }
